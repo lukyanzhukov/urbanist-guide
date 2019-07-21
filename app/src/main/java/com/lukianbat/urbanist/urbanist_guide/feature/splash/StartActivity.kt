4efa515ed2e490a7d7a6f9af1f79a71f88795991
lukianbat <lukianzhukov2000@gmail.com>
2019-07-22 00:26:11 +0700

    some changes

--- app/src/main/java/com/lukianbat/urbanist/urbanist_guide/feature/splash/StartActivity.kt
+++ app/src/main/java/com/lukianbat/urbanist/urbanist_guide/feature/splash/StartActivity.kt
@@ -1,4 +1,4 @@
-package com.lukianbat.urbanist.urbanist_guide.feature.start
+package com.lukianbat.urbanist.urbanist_guide.feature.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/main/java/com/lukianbat/urbanist/urbanist_guide/сore/App.kt
+++ app/src/main/java/com/lukianbat/urbanist/urbanist_guide/сore/App.kt
@@ -1,4 +1,7 @@
 package com.lukianbat.urbanist.urbanist_guide.сore
 
+import com.google.maps.model.LatLng
+
 class App {
+    val d = LatLng()
 }
